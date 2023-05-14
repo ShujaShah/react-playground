0c83024ce4227b9d77632379c990a59a5a263466
ShujaShah <shuja.ashraf11@gmail.com>
2023-05-14 16:06:30 +0530

    added the parameters

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import usePosts from "../hooks/usePosts";
 
 const posts = () => {
-  const { data: posts, error, isLoading } = usePosts();
+  const [userId, setUserId] = useState<number>();
+  const { data: posts, error, isLoading } = usePosts(userId);
 
   return (
     <>
       {error && <p>{error.message}</p>}
       {isLoading && <p>Loading...</p>}
+      <select onChange={(event) => setUserId(parseInt(event.target.value))} className="form-select" value={userId}>
+        <option value=""></option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
       <ul>
         {posts?.map((post) => (
           <li key={post.id}>{post.title}</li>
--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -6,12 +6,12 @@ interface Post {
   title: string;
 }
 
-const usePosts = () => {
+const usePosts = (userId: number | undefined) => {
   const fetchPosts = () => {
-    return axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts").then((res) => res.data);
+    return axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts",{params: {userId}}).then((res) => res.data);
   };
 return useQuery<Post[], Error>({
-    queryKey: ["posts"], //unique identifier which is array of one or more values...
+    queryKey: ["users", userId, "posts"], //unique identifier which is array of one or more values...
     queryFn: fetchPosts, // function that we use to fetch the data from the backend
     staleTime: 10*1000  // how long the data is considered to be fresh in this case 10 seconds 
 });
