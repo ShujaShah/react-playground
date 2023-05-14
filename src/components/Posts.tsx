85905cc21f0761a2f486a24ed4632b3790e16380
ShujaShah <shuja.ashraf11@gmail.com>
2023-05-14 16:32:04 +0530

    added the pagination

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -2,24 +2,32 @@ import { useState } from "react";
 import usePosts from "../hooks/usePosts";
 
 const posts = () => {
-  const [userId, setUserId] = useState<number>();
-  const { data: posts, error, isLoading } = usePosts(userId);
+  const pageSize = 10;
+  const [page, setPage] = useState(1);
+  //const [userId, setUserId] = useState<number>();
+  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
 
   return (
     <>
       {error && <p>{error.message}</p>}
       {isLoading && <p>Loading...</p>}
-      <select onChange={(event) => setUserId(parseInt(event.target.value))} className="form-select" value={userId}>
+      {/* <select onChange={(event) => setUserId(parseInt(event.target.value))} className="form-select" value={userId}>
         <option value=""></option>
         <option value="1">User 1</option>
         <option value="2">User 2</option>
         <option value="3">User 3</option>
-      </select>
+      </select> */}
       <ul>
         {posts?.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
+      <button disabled={page === 1} className="btn btn-primary" onClick={() => setPage(page - 1)}>
+        Prev
+      </button>
+      <button className="btn btn-primary" onClick={() => setPage(page + 1)}>
+        Next
+      </button>
     </>
   );
 };
--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -6,14 +6,23 @@ interface Post {
   title: string;
 }
 
-const usePosts = (userId: number | undefined) => {
+interface PostQuery{
+    page: number,
+    pageSize: number
+}
+
+const usePosts = (query: PostQuery) => {
   const fetchPosts = () => {
-    return axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts",{params: {userId}}).then((res) => res.data);
+    return axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts",{params: {
+        _start: (query.page-1) * query.pageSize,
+        _limit: (query.pageSize)
+    }}).then((res) => res.data);
   };
 return useQuery<Post[], Error>({
-    queryKey: ["users", userId, "posts"], //unique identifier which is array of one or more values...
+    queryKey: ["posts", query], //unique identifier which is array of one or more values...
     queryFn: fetchPosts, // function that we use to fetch the data from the backend
-    staleTime: 10*1000  // how long the data is considered to be fresh in this case 10 seconds 
+    staleTime: 10*1000 , // how long the data is considered to be fresh in this case 10 seconds
+    keepPreviousData: true, 
 });
 };
 
